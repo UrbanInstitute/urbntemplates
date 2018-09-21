d9ede1851dac13514541f1cc54332a12a987ff06
awunderground <aaronrwilliams100@gmail.com>
2018-09-21 17:43:04 -0400

    Restructure use_git_ignore_urbn().

--- R/use_git_ignore_urbn.R
+++ R/use_git_ignore_urbn.R
@@ -11,27 +11,59 @@
 #' @export
 use_git_ignore_urbn <- function(gitignore = NULL, open = FALSE) {
 
-  # add template to destination
-  usethis::use_template(
-    template = ".gitignore",
-    save_as = ".gitignore",
-    data = list(Package = "", Version = ""),
-    ignore = FALSE,
-    open = open,
-    package = "urbntemplates"
-  )
+ignore <-
+"# History files
+.Rhistory
+.Rapp.history
+
+# Session Data files
+.RData
+
+# Example code in package build process
+*-Ex.R
+
+# Output files from R CMD build
+/*.tar.gz
+
+# Output files from R CMD check
+/*.Rcheck/
+
+# RStudio files
+.Rproj.user/
+
+# produced vignettes
+vignettes/*.html
+vignettes/*.pdf
+
+# OAuth2 token, see https://github.com/hadley/httr/releases/tag/v0.3
+.httr-oauth
+
+# knitr and R markdown default cache directories
+/*_cache/
+/cache/
+
+# Temporary files created by R markdown
+*.utf8.md
+*.knit.md"
+
+  usethis::use_git_ignore(ignore)
 
   # pick a gitignore template
   template_name <- if (is.null(gitignore)) {
 
-  } else if (gitignore %in% c("shiny", "fact_sheet_html", "fact_sheet_pdf", "web_report")) {
+  } else if (gitignore == "fact_sheet_pdf") {
+
+    usethis::use_git_ignore("*.aux")
+    usethis::use_git_ignore("*.out")
+
+  }else if (gitignore %in% c("shiny", "fact_sheet_html", "web_report")) {
 
     # usethis::use_git_ignore()
 
   } else {
 
-    stop("Invalid 'gitignore' argument. Valid gitignore options are: ",
-         "shiny",
+    stop("Invalid 'gitignore' argument. Valid 'gitignore' options are: ",
+         "'shiny', 'fact_sheet_html', 'fact_sheet_pdf', 'web_report'",
          call. = FALSE)
 
   }
